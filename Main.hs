6b4ea194f23f4bad950f78f3b27946f85ae5ac61
Felipe Bormann <felipe.bormann@gmail.com>
2016-10-12 03:29:27 -0300

    Added For Loop and started UnaryAssignOperator

--- Main.hs
+++ Main.hs
@@ -10,6 +10,15 @@ import Value
 -- Evaluate functions
 --
 
+evalForInit :: StateT -> ForInit -> StateTransformer Value
+evalForInit env (NoInit) = return Nil
+evalForInit env (VarInit []) = return Nil
+evalForInit env (VarInit (x:xs)) = do
+    varDecl env x >> evalForInit env (VarInit xs)
+
+evalForInit env (ExprInit expr) = do
+    evalExpr env expr
+
 evalExpr :: StateT -> Expression -> StateTransformer Value
 evalExpr env (NumLit d) = return $ Double d
 evalExpr env (VarRef (Id id)) = stateLookup env id
@@ -30,52 +39,57 @@ evalExpr env (ArrayLit l) = evalArray env l (Array [])
 
 evalExpr env (ListExpr []) = return Nil
 evalExpr env (ListExpr (l:ls)) = do
-    					evalExpr env l >> evalExpr env (ListExpr ls)
+                        evalExpr env l >> evalExpr env (ListExpr ls)
+evalExpr env (UnaryAssignExpr op (LVar var)) = do
+    stateLookup env var -- return error if the variable doesn`t exist
+    e <- evalExpr env var
+    unaryAssignOp env op value
+
 
 
 
 evalStmt :: StateT -> Statement -> StateTransformer Value
 evalStmt env (BlockStmt []) = return Nil
 evalStmt env (BlockStmt (x:xs) ) = do
-					v1 <- evalStmt env x
-					if (v1 == (Break) || v1 == (Continue)) then
-						return v1
-					else
-						evalStmt env (BlockStmt xs)
+                    v1 <- evalStmt env x
+                    if (v1 == (Break) || v1 == (Continue)) then
+                        return v1
+                    else
+                        evalStmt env (BlockStmt xs)
 evalStmt env (IfStmt cond c1 c2) = do
-					v1 <- evalExpr env cond
-					if (v1 == (Bool True)) then
-						evalStmt env c1
-					else if (v1 == (Bool False)) then
-						evalStmt env c2
-					else return Nil
+                    v1 <- evalExpr env cond
+                    if (v1 == (Bool True)) then
+                        evalStmt env c1
+                    else if (v1 == (Bool False)) then
+                        evalStmt env c2
+                    else return Nil
 evalStmt env (IfSingleStmt cond c1) = do
-					v1 <- evalExpr env cond
-					if (v1 == (Bool True)) then
-						evalStmt env c1
-					else return Nil
+                    v1 <- evalExpr env cond
+                    if (v1 == (Bool True)) then
+                        evalStmt env c1
+                    else return Nil
 evalStmt env (WhileStmt cond c1) = do
-					v1 <- evalExpr env cond
-					if (v1 == (Bool True)) then
-						do
-							v2 <- evalStmt env c1
-							if (v2 == (Break)) then
-								return Nil
-							else
-								evalStmt env (WhileStmt cond c1)
-					else 
-						return Nil
-					
+                    v1 <- evalExpr env cond
+                    if (v1 == (Bool True)) then
+                        do
+                            v2 <- evalStmt env c1
+                            if (v2 == (Break)) then
+                                return Nil
+                            else
+                                evalStmt env (WhileStmt cond c1)
+                    else 
+                        return Nil
+                    
 evalStmt env (DoWhileStmt c1 cond) = do
-					v1 <- evalStmt env c1
-					if (v1 == (Break)) then
-						return Nil
-					else
-						do
-							v2 <- evalExpr env cond
-							if (v2 == (Bool True)) then
-								evalStmt env (DoWhileStmt c1 cond)
-							else return Nil
+                    v1 <- evalStmt env c1
+                    if (v1 == (Break)) then
+                        return Nil
+                    else
+                        do
+                            v2 <- evalExpr env cond
+                            if (v2 == (Bool True)) then
+                                evalStmt env (DoWhileStmt c1 cond)
+                            else return Nil
 
 
 
@@ -92,7 +106,22 @@ evalStmt env (VarDeclStmt []) = return Nil
 evalStmt env (VarDeclStmt (decl:ds)) =
     varDecl env decl >> evalStmt env (VarDeclStmt ds)
 evalStmt env (ExprStmt expr) = evalExpr env expr
-
+evalStmt env (ForStmt initExpression cond afterblock block) = do
+                                        evalForInit env initExpression
+                                        case cond of
+                                            Nothing -> return (Return Nil)
+                                            (Just  expr) -> do
+                                                checkCond <- evalExpr env expr
+                                                if (checkCond == (Bool True)) then
+                                                    do 
+                                                        evalStmt env block
+                                                        case afterblock of
+                                                            Nothing -> return (Return Nil)
+                                                            (Just expr) -> do
+                                                                evalExpr env expr
+                                                               
+                                                        evalStmt env (ForStmt NoInit cond afterblock block)
+                                                else return Nil
 
 -- Do not touch this one :)
 evaluate :: StateT -> [Statement] -> StateTransformer Value
@@ -120,6 +149,13 @@ infixOp env OpNEq  (Int v1) (Int v2) = return $ Bool $ v1 /= v2
 infixOp env OpLAnd (Bool v1) (Bool v2) = return $ Bool $ v1 && v2
 infixOp env OpLOr  (Bool v1) (Bool v2) = return $ Bool $ v1 || v2
 
+unaryAssignOp :: StateT -> UnaryAssignOp -> Value -> StateTransformer Value
+unaryAssignOp env PostfixInc (Int v1) = return $ Int $ v1 + 1
+unaryAssignOp env PostfixDec (Int v1) = return $ Int $ v1 - 1
+unaryAssignOp env PrefixInc (Int v1) = return $ Int $ v1 + 1
+unaryAssignOp env PrefixDec (Int v1) = return $ Int $ v1 - 1
+
+
 --
 -- Environment and auxiliary functions
 --
@@ -131,7 +167,7 @@ stateLookup :: StateT -> String -> StateTransformer Value
 stateLookup env var = ST $ \s ->
     -- this way the error won't be skipped by lazy evaluation
     case Map.lookup var (union s env) of
-        Nothing -> error $ "Variable " ++ show var ++ " not defiend."
+        Nothing -> error $ "Variable " ++ show var ++ " not defined."
         Just val -> (val, s)
 
 varDecl :: StateT -> VarDecl -> StateTransformer Value
@@ -144,6 +180,7 @@ varDecl env (VarDecl (Id id) maybeExpr) = do
 
 setVar :: String -> Value -> StateTransformer Value
 setVar var val = ST $ \s -> (val, insert var val s)
+
 
 evalArray :: StateT -> [Expression] -> Value -> StateTransformer Value
 evalArray env [] (Array l) = return (Array l)
